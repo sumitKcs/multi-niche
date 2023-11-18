c03ecf6db4830775a99e408cd76dbd5a5293643d
sumit kumar <itssumit258@gmail.com>
2023-11-18 16:31:36 +0530

    feat. working on tab megamenu

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,3 +14,27 @@ sidebar_close_button.addEventListener("click", () => {
   document.body.classList.remove("no-scroll");
 });
 
+window.addEventListener("DOMContentLoaded", () => {
+  const next = document.querySelectorAll(".next");
+  const prev = document.querySelectorAll(".prev");
+
+  next_elements = Array.from(next);
+  next_elements.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      const carousel = element.parentElement.parentElement.firstElementChild;
+      let first_element_width =
+        carousel.parentElement.firstElementChild.firstElementChild.clientWidth;
+      carousel.scrollLeft += first_element_width;
+    });
+  });
+
+  prev_elements = Array.from(prev);
+  prev_elements.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      const carousel = element.parentElement.parentElement.firstElementChild;
+      const first_element_width =
+        carousel.parentElement.firstElementChild.firstElementChild.clientWidth;
+      carousel.scrollLeft -= first_element_width;
+    });
+  });
+});
